56df98c1c3e3cb390af09d088abaedf7b05f2fc5
DutraGames <gabrieldutraamarante@gmail.com>
2024-01-18 19:11:47 -0300

    refactor: add type url in sprite

--- src/schemas/pokeSchema/index.ts
+++ src/schemas/pokeSchema/index.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const pokemonSchema = z.object({
     name: z.string(),
     types: z.array(z.object({ type: z.object({ name: z.string() }) })),
-    sprites: z.object({ front_default: z.string() })
+    sprites: z.object({ front_default: z.string().url() })
 });
