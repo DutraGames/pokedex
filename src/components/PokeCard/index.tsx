c85a6dbfcf161f16f90eefd4884e0108932b093a
DutraGames <gabrieldutraamarante@gmail.com>
2024-01-18 17:53:51 -0300

    refactor: finalized component

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -3,9 +3,22 @@ import React, { useState, useEffect } from 'react';
 import { pokemonSchema } from '@/schemas/pokeSchema';
 import { api } from '@/utils/api';
 import { IPokemon } from '@/@types/pokemon';
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+
 export default function PokeCard() {
     const [data, setData] = useState<IPokemon | null>(null);
-    const [id, setId] = useState<number>(25);
+    const [id, setId] = useState<number>(1);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -22,20 +35,43 @@ export default function PokeCard() {
     }, [id]);
 
     return (
-        <div>
-            {data && (
-                <div>
-                    <h1>{data.name}</h1>
-                    <p>
-                        Types:{' '}
-                        {data.types.map((type) => type.type.name).join(', ')}
-                    </p>
-                    <img
-                        src={data.sprites.front_default}
-                        alt={`${data.name} sprite`}
-                    />
-                </div>
+        <Card
+            className={cn(
+                'w-96 h-96 flex items-center flex-col justify-center'
             )}
-        </div>
+        >
+            {data && (
+                <>
+                    <CardHeader className={cn('w-full flex flex-col gap-1')}>
+                        <CardTitle className={cn('text-primary text-center')}>
+                            {id} - {data.name}
+                        </CardTitle>
+                        <CardDescription
+                            className={cn('gap-4 flex justify-center')}
+                        >
+                            {data.types.map((type) => (
+                                <Badge variant="outline" key={type.type.name}>
+                                    {type.type.name}{' '}
+                                </Badge>
+                            ))}
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent
+                        className={cn(
+                            'w-full flex items-center justify-center h-full'
+                        )}
+                    >
+                        <Avatar className={cn('w-48 h-48')}>
+                            <AvatarImage src={data.sprites.front_default} />
+                            <AvatarFallback>Picachu</AvatarFallback>
+                        </Avatar>
+                    </CardContent>
+                    <CardFooter className={cn('gap-2')}>
+                        <Button onClick={() => setId(id - 1)}>Prev</Button>
+                        <Button onClick={() => setId(id + 1)}>Next</Button>
+                    </CardFooter>
+                </>
+            )}
+        </Card>
     );
 }
