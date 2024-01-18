fbe59aa1172c3087be381b65b3774a019338e585
DutraGames <gabrieldutraamarante@gmail.com>
2024-01-18 17:54:38 -0300

    refactor: remove struct card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,39 +17,6 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center gap-4 p-24 w-screen">
             <h1 className="text-5xl font-bold text-primary">Pokedex</h1>
-
-            <Card
-                className={cn(
-                    'w-96 h-96 flex items-center flex-col justify-center'
-                )}
-            >
-                <CardHeader className={cn('w-full flex flex-col gap-1')}>
-                    <CardTitle className={cn('text-primary text-center')}>
-                        25 - Pikachu
-                    </CardTitle>
-                    <CardDescription
-                        className={cn('gap-4 flex justify-center')}
-                    >
-                        <Badge variant="outline">eletric</Badge>
-                        <Badge variant="outline">poison</Badge>
-                    </CardDescription>
-                </CardHeader>
-                <CardContent
-                    className={cn(
-                        'w-full flex items-center justify-center h-full'
-                    )}
-                >
-                    <Avatar className={cn('w-48 h-48')}>
-                        <AvatarImage src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
-                        <AvatarFallback>Picachu</AvatarFallback>
-                    </Avatar>
-                </CardContent>
-                <CardFooter className={cn('gap-2')}>
-                    <Button>Prev</Button>
-                    <Button>Next</Button>
-                </CardFooter>
-            </Card>
-
             <PokeCard />
         </main>
     );
